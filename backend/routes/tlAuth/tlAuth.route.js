44ab452e90f095371d2b307e1fb89aad2245969f
blackmamoth <blackmamoth83@gmail.com>
2023-01-27 22:44:37 +0530

    Addess refresh route in Tl's auth

--- backend/routes/tlAuth/tlAuth.route.js
+++ backend/routes/tlAuth/tlAuth.route.js
@@ -3,5 +3,6 @@ const TlAuthController = require("../../controllers/tlAuth/auth.controller");
 
 router.post("/register", TlAuthController.registerThreadLord);
 router.post("/login", TlAuthController.loginThreadLord);
+router.post("/refresh", TlAuthController.refreshThreadLordToken);
 
 module.exports = router;
